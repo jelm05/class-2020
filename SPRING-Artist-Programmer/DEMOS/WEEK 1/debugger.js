99a41ec0bfc6d53aa9506aa974a4b870b14a4395
Justin Elm <justinelm@me.com>
2020-01-17 16:53:18 -0500

    Spring 2020, first week, syllabus, slides, debugging

---
+++ SPRING-Artist-Programmer/DEMOS/WEEK 1/debugger.js
@@ -0,0 +1,17 @@
+function replaceTheWords() {
+
+  const replacementWords = "Replaced!";
+
+  // Look at the docuemnt and find the element that has
+  // an ID of "demo"
+  document.getElementById("demo").innerHTML = replacementWords;
+
+}
+
+let tempNum = "1";
+
+console.log(typeof(tempNum));
+// replaceTheWords();
+ // let objectOne = {
+ //   name: Justin
+ // }
